511cd51f45af1de0ffddcf40380839610cca1be2
DanielBarragan96 <ie702554@iteso.mx>
2020-10-28 21:16:42 -0600

    completed reto 2

--- s2/reto_2.sql
+++ s2/reto_2.sql
@@ -5,11 +5,10 @@ USE tienda;
 SHOW tables;
 
 #¿Cuál es el promedio de salario de los puestos?
-SELECT AVG(salario) FROM puesto;
+SELECT ROUND(AVG(salario), 2) FROM puesto;
 #¿Cuántos artículos incluyen la palabra Pasta en su nombre?
 SELECT count(*) FROM articulo WHERE nombre LIKE "%pasta%";
 #¿Cuál es el salario mínimo y máximo?
-SELECT MIN(salario) FROM puesto;
-SELECT MAX(salario) FROM puesto;
+SELECT MIN(salario), MAX(salario) FROM puesto;
 #¿Cuál es la suma del salario de los últimos cinco puestos agregados?
-SELECT SUM(salario) FROM puesto ORDER BY id_puesto DESC LIMIT 5;
+select sum(salario) as "Suma ultimos 5 salarios" from (select salario from puesto order by id_puesto desc limit 5) as last5Salario;
