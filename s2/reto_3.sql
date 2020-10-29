451e503b535fee5acf76c356f5ca38a559fbf3b7
DanielBarragan96 <ie702554@iteso.mx>
2020-10-28 20:52:11 -0600

    reto 3 initial commit

---
+++ s2/reto_3.sql
@@ -0,0 +1,13 @@
+#Daniel Barragán
+#https://github.com/beduExpert/A1-Introduccion-a-Bases-de-Datos-Santander/tree/main/Sesion-02/Reto-03
+
+USE tienda;
+SHOW tables;
+
+#¿Cuántos registros hay por cada uno de los puestos?
+
+#¿Cuánto dinero se paga en total por puesto?
+
+#¿Cuál es el número total de ventas por vendedor?
+
+#¿Cuál es el número total de ventas por artículo?
