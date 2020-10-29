d904307975138b7d7a64127c8f05b58dba1cee3a
DanielBarragan96 <ie702554@iteso.mx>
2020-10-28 21:00:05 -0600

    completed reto 3

--- s2/reto_3.sql
+++ s2/reto_3.sql
@@ -5,9 +5,10 @@ USE tienda;
 SHOW tables;
 
 #¿Cuántos registros hay por cada uno de los puestos?
-
+SELECT nombre, count(*) FROM puesto GROUP BY nombre;
 #¿Cuánto dinero se paga en total por puesto?
-
+SELECT nombre, sum(salario) FROM puesto GROUP BY nombre;
 #¿Cuál es el número total de ventas por vendedor?
-
+SELECT id_empleado, count(id_venta) FROM venta GROUP BY id_empleado;
 #¿Cuál es el número total de ventas por artículo?
+SELECT id_articulo, count(id_venta) FROM venta GROUP BY id_articulo;
