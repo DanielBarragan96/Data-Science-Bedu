7325b7a0bae772358f36a1c7177bbb60549c79be
DanielBarragan96 <ie702554@iteso.mx>
2020-10-28 20:32:19 -0600

    starting reto

---
+++ s2/reto_2.sql
@@ -0,0 +1,13 @@
+#Daniel Barragán
+#https://github.com/beduExpert/A1-Introduccion-a-Bases-de-Datos-Santander/tree/main/Sesion-02/Reto-02
+
+USE tienda;
+SHOW tables;
+
+#¿Cuál es el promedio de salario de los puestos?
+SELECT salario FROM puesto;
+#¿Cuántos artículos incluyen la palabra Pasta en su nombre?
+
+#¿Cuál es el salario mínimo y máximo?
+
+#¿Cuál es la suma del salario de los últimos cinco puestos agregados?
