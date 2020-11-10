83b91e51b9bf779dbf49a96159e74a8851539ce2
DanielBarragan96 <ie702554@iteso.mx>
2020-11-09 22:13:02 -0600

    s4 r2

---
+++ s4/reto_2/1.comments_greg.py
@@ -0,0 +1,11 @@
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+client = MongoClient('mongodb+srv://beduUser:12345@beducluster.0duqi.mongodb.net/test?authSource=admin&replicaSet=atlas-k7nqky-shard-0&connectTimeoutMS=600000&socketTimeoutMS=6000000&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+filter={
+    'name': 'Greg Powell'
+}
+
+result = client['sample_mflix']['comments'].find(
+  filter=filter
+)
---
+++ s4/reto_2/3.movies_maxComments.py
@@ -0,0 +1,19 @@
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+client = MongoClient('mongodb+srv://beduUser:12345@beducluster.0duqi.mongodb.net/test?authSource=admin&replicaSet=atlas-k7nqky-shard-0&connectTimeoutMS=600000&socketTimeoutMS=6000000&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+filter={}
+project={
+    'num_mflix_comments': 1
+}
+sort=list({
+    'num_mflix_comments': -1
+}.items())
+limit=1
+
+result = client['sample_mflix']['movies'].find(
+  filter=filter,
+  projection=project,
+  sort=sort,
+  limit=limit
+)
